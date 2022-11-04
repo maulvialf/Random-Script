6daaf226caa53ebf741c22569db3ce2824544eb0
Maulvi Alfansuri <alfakatsuki@gmail.com>
2022-11-04 20:14:59 +0700

    Update utils.js

--- utils.js
+++ utils.js
@@ -5,3 +5,11 @@ function jsonDumps(params) {
 function jsonLoads(params) {
     return JSON.parse(params)    
 }
+
+
+// Convert a number to a hexadecimal string with:
+
+hexString = yourNumber.toString(16);
+// And reverse the process with:
+
+yourNumber = parseInt(hexString, 16);
