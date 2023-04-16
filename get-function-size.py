b958d680e8c80dde5cbdf161390200dffcacfcce
Maulvi Alfansuri <alfakatsuki@gmail.com>
2023-04-16 09:16:55 +0700

    Update get-function-size.py

--- get-function-size.py
+++ get-function-size.py
@@ -3,4 +3,5 @@ r2 = r2pipe.open('./out')
 r2.cmd('aaa')
 function_info = r2.cmdj(f'afij sym.validate')
 function_size = function_info[0]['size']
-function_offset = function_info[0]['offset'] r2.quit()
+function_offset = function_info[0]['offset']
+r2.quit()
