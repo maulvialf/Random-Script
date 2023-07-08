0378f30d3400699a0d5fea5254e8f11efafe1218
Maulvi Alfansuri <alfakatsuki@gmail.com>
2023-07-09 05:07:22 +0700

    Update gdbscript-snippet.py

--- gdbscript-snippet.py
+++ gdbscript-snippet.py
@@ -14,6 +14,32 @@ def extract_mem(parammem):
 	print('extract', hex(extractval))
 	return extractval
 
+# Function to extract memory using GDB
+def extract_memory(address, num_bytes):
+    # Run GDB in non-interactive mode
+    # gdb.execute("set pagination off")
+    # gdb.execute("set logging on")
+
+    # Execute the x/16bx command to extract memory
+    gdb_output = gdb.execute("x/{}bx {}".format(num_bytes, address), to_string=True)
+
+    # Parse the GDB output to extract the bytes
+    byte_array = []
+    lines = gdb_output.strip().split("\n")
+    for line in lines:
+        byte_str = line.split(":")[1].strip()
+        byte_str = byte_str.replace("0x", "")
+        byte_str = byte_str.replace("\t", "")
+        byte = int(byte_str, 16)
+        byte_array.append(byte)
+
+    # Save the output to a log file
+    # gdb.execute("quit", to_string=True)
+
+    return byte_array
+
+data = extract_memory(0x555555559483, 1)
+
 mem1 = extract_mem("$rbp")
 mem2 = extract_mem("0x7fffffff00000")
 reg1 = gdb.parse_and_eval("$eax")
